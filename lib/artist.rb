99dc65498b7c5535e478592565e1dd11446f5de7
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-19 15:23:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist 
   
-  attr_accessor :name
+  attr_accessor :name, :songs
   
   def initialize(name)
     @name = name 
@@ -18,7 +18,7 @@ class Artist
   
   def add_song_by_name(song)
     song = Song.new(name)
-    song.artist = self
+    add_song(song)
   end 
   
   def self.song_count
